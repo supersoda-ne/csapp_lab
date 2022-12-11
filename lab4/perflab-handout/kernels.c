4ae4c704586340a4d57b779077ca00d6adc351c1
supersoda-ne <kxtcyh@163.com>
2022-12-11 04:50:35 -0800

    lab4 finished

--- lab4/perflab-handout/kernels.c
+++ lab4/perflab-handout/kernels.c
@@ -46,19 +46,6 @@ void naive_rotate(int dim, pixel *src, pixel *dst)
  * IMPORTANT: This is the version you will be graded on
  */
 char rotate_descr[] = "rotate: Current working version";
-// void rotate(int dim, pixel *src, pixel *dst) 
-// {
-//     int i, j;
-//     int ii, jj;
-//     int ts, td;
-//     pixel *ps=src;
-//     for (i = 0; i < dim; i += 1) {
-//         // ps = src + i;
-//         for (j = 0; j < dim; j += 1) {
-//             dst[RIDX(dim-1-j, i, dim)] = *ps++;
-//         }
-//     }
-// }
 
 void rotate(int dim, pixel *src, pixel *dst) 
 {
@@ -68,10 +55,7 @@ void rotate(int dim, pixel *src, pixel *dst)
     pixel *ps, *pd;
     for (i = 0; i < dim; i += 8) {
         for (j = 0; j < dim; j += 8) {
-            
             for (ii = i; ii < i + 8; ii += 1) {
-                // ts = ii * dim + j;
-                // td = (dim-1-j) * dim + ii;
                 pd = dst + (dim-1-j)*dim+ii;
                 ps = src + ii*dim+j;
                 *pd = *ps++;
@@ -90,25 +74,6 @@ void rotate(int dim, pixel *src, pixel *dst)
                 *pd = *ps++;
                 pd -= dim;
                 *pd = *ps;
-
-                // td = (dim-1-j)*dim+ii;
-                // ts = ii*dim+j;
-                // dst[td - 0 * dim] = src[ts+0];
-                // dst[td - 1 * dim] = src[ts+1];
-                // dst[td - 2 * dim] = src[ts+2];
-                // dst[td - 3 * dim] = src[ts+3];
-                // dst[td - 4 * dim] = src[ts+4];
-                // dst[td - 5 * dim] = src[ts+5];
-                // dst[td - 6 * dim] = src[ts+6];
-                // dst[td - 7 * dim] = src[ts+7];
-
-                // for (jj = j; jj < j + ROTATE_BATCH; jj += 1) {
-                //     // dst[td] = src[ts];
-                //     // td -= dim;
-                //     // ts += 1;
-                //     dst[(dim-1-jj)*dim+ii] = src[ii*dim+jj];
-                    
-                // }
             }
         }
     }
@@ -125,7 +90,7 @@ void rotate(int dim, pixel *src, pixel *dst)
 void register_rotate_functions() 
 {
     // add_rotate_function(&naive_rotate, naive_rotate_descr);   
-    // add_rotate_function(&rotate, rotate_descr);   
+    add_rotate_function(&rotate, rotate_descr);   
     /* ... Register additional test functions here */
 }
 
@@ -275,12 +240,6 @@ static void assign_avg_to_pixel(pixel *current_pixel, pixel_sum sum, int cnt)
     current_pixel->blue = (unsigned short) (sum.blue/cnt);
     return;
 }
-static void pixel_to_str(char *s, pixel x) {
-    sprintf(s, "(%d, %d, %d)", x.red, x.green, x.blue);
-}
-static void sum_to_str(char *s, pixel_sum x) {
-    sprintf(s, "(%d, %d, %d)", x.red, x.green, x.blue);
-}
 
 pixel_sum row_sum_space[SMOOTH_N+2][SMOOTH_N+2]={0};
 pixel_sum *row_sum = &row_sum_space[1][1];
@@ -343,19 +302,9 @@ static void sum_3x2(pixel *res, int dim, pixel *src) {
  * IMPORTANT: This is the version you will be graded on
  */
 char smooth_descr[] = "smooth: Current working version";
-
-char str0[100];
-char str1[100];
-char str2[100];
 void smooth(int dim, pixel *src, pixel *dst) 
 {   
     int i, j;
-    // printf("$%d$\n", (int)(src-10)->red);
-    // exit(0);
-    // naive_smooth(dim, src, dst);
-    
-    // pixel_sum s;
-
     for(i = 0; i < dim; i += SMOOTH_N) {
         for(j = 0; j < dim; j += SMOOTH_N) {
             smooth_NxN(dim, src + RIDX(i, j, dim), dst + RIDX(i, j, dim));
@@ -364,22 +313,19 @@ void smooth(int dim, pixel *src, pixel *dst)
     // top
     for(i = 1; i < dim-1; i += 1) {
         sum_2x3(dst + RIDX(0, i, dim), dim, src + RIDX(0, i-1, dim));
-        sum_2x3(dst + RIDX(dim-1, i, dim), dim, src + RIDX(dim-2, i-1, dim));
-        sum_3x2(dst + RIDX(i, 0, dim), dim, src + RIDX(i-1, 0, dim));
-        sum_3x2(dst + RIDX(i, dim-1, dim), dim, src + RIDX(i-1, dim-2, dim));
-
     }
-    // // bottom
-    // for(i = 1; i < dim-1; i += 1) {
-        
-    // }
-    // // left
-    // for(i = 1; i < dim-1; i += 1) {
-    // }
-    // // right
-    // for(i = 1; i < dim-1; i += 1) {
-        
-    // }
+    // bottom
+    for(i = 1; i < dim-1; i += 1) {
+        sum_2x3(dst + RIDX(dim-1, i, dim), dim, src + RIDX(dim-2, i-1, dim));
+    }
+    // left
+    for(i = 1; i < dim-1; i += 1) {
+        sum_3x2(dst + RIDX(i, 0, dim), dim, src + RIDX(i-1, 0, dim));
+    }
+    // right
+    for(i = 1; i < dim-1; i += 1) {
+        sum_3x2(dst + RIDX(i, dim-1, dim), dim, src + RIDX(i-1, dim-2, dim));
+    }
     // top-left
     sum_2x2(dst + RIDX(0,0,dim), dim, src + RIDX(0,0,dim));
     // top-right
@@ -388,7 +334,6 @@ void smooth(int dim, pixel *src, pixel *dst)
     sum_2x2(dst + RIDX(dim-1,0,dim), dim, src + RIDX(dim-2,0,dim));
     // bottom-right
     sum_2x2(dst + RIDX(dim-1,dim-1,dim), dim, src + RIDX(dim-2,dim-2,dim));
-
 }
 
 
